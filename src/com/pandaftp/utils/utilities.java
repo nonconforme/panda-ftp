498d0ad8b447704cf887aff05bee8697fe4547a5
Jfollett88 <Jfollett88@gmail.com>
2012-04-01 08:41:22 +0000

    Can Create new browsers for loacl files with new objects or can browse using static class. please remember to use the "/" as the begining class when Using class

--- src/com/pandaftp/utils/utilities.java
+++ src/com/pandaftp/utils/utilities.java
@@ -2,6 +2,7 @@ package com.pandaftp.utils;
 
 import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.lang.reflect.Array;
 
 import javax.annotation.Resource;
 
@@ -14,6 +15,7 @@ import com.pandaftp.main.R;
 
 public class utilities {
 
+	//Displays Errors Located in Strings Allows for later on translations
 	public static void errorbox(int code,Context c){
 		
 		String[] items = c.getResources().getStringArray(R.array.Error);
@@ -45,19 +47,58 @@ public class utilities {
 		}
 		return result;
 	}
-	
-	public static void localfiles(final Context c)
+	//Lets you Browse Local Directory
+	public static void localfiles(final Context c, final String Directory)
 	{
-		File dir = new File("/");
+		File dir = new File(Directory);
 		final String[] files = dir.list();
 		
 		AlertDialog.Builder builder = new AlertDialog.Builder(c);
-		builder.setTitle("Pick a File");
+		builder.setTitle("Pick a File")
+				.setCancelable(true);
+		builder.setItems(files, new DialogInterface.OnClickListener() {
+		    public void onClick(DialogInterface dialog, int item) {
+		        Toast.makeText(c, files[item], Toast.LENGTH_SHORT).show();
+		        if(!isFile(files[item])){
+		        String Direct = Directory+ files[item];
+		        utilities.localfiles(c,Direct);
+		        }
+		    }
+		})
+		
+		.show();
+	}
+	//If wanting to create new Browsing directory.
+	public void browse(final Context c,String Directory)
+	{
+		File dir = new File(Directory);
+		final String[] files = dir.list();
+		
+		AlertDialog.Builder builder = new AlertDialog.Builder(c);
+		builder.setTitle("Pick a File")
+				.setCancelable(true);
 		builder.setItems(files, new DialogInterface.OnClickListener() {
 		    public void onClick(DialogInterface dialog, int item) {
 		        Toast.makeText(c, files[item], Toast.LENGTH_SHORT).show();
 		    }
-		}).show();
+		})
+		
+		.show();
 	}
-	
+	//Notifys if its a file or a Directory
+	private static boolean isFile(String selection)
+	{
+		boolean is =false;
+		char[] selectionArray = selection.toCharArray();
+		
+		for(int i =0;i < selectionArray.length;i++ )
+		{
+			if(selectionArray[i]=='.')
+			{
+				is =true;
+			}
+		}
+		
+		return is;
+	} 
 }
