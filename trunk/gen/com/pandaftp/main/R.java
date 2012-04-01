56cf07a824d1d27b4dae4e96f040e09afd1b4969
jakejamessteele@gmail.com <jakejamessteele@gmail.com@f50129a6-5029-db52-7855-f07b842d22e5>
2012-04-01 03:35:59 +0000

    Added my List Activity Back Without Code

--- trunk/gen/com/pandaftp/main/R.java
+++ trunk/gen/com/pandaftp/main/R.java
@@ -62,8 +62,13 @@ public final class R {
         public static final int xml=0x7f020030;
         public static final int zip=0x7f020031;
     }
+    public static final class id {
+        public static final int icon=0x7f050000;
+        public static final int label=0x7f050001;
+    }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int dirlist=0x7f030000;
+        public static final int main=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
---
+++ trunk/src/com/pandaftp/main/directory_list.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.pandaftp.main;
+
+/**
+ * @author jacob
+ *
+ */
+public class directory_list {
+
+}
