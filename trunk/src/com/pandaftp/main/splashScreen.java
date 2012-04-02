ad70777254332187449d8be4d5cd2f7da822458f
Jfollett88@gmail.com <Jfollett88@gmail.com@f50129a6-5029-db52-7855-f07b842d22e5>
2012-04-02 00:42:24 +0000

    Created Splash Screen (Without Designed Layout)

--- trunk/src/com/pandaftp/main/splashScreen.java
+++ trunk/src/com/pandaftp/main/splashScreen.java
@@ -1,13 +1,46 @@
+//Contributed Code help from DroidNova.com
 package com.pandaftp.main;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 
 public class splashScreen extends Activity {
-    /** Called when the activity is first created. */
+	protected boolean _active = true;
+	protected int _splashTime = 5000;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.splashscreen);
+        Thread splashTread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    int waited = 0;
+                    while(_active && (waited < _splashTime)) {
+                        sleep(100);
+                        if(_active) {
+                            waited += 100;
+                        }
+                    }
+                } catch(InterruptedException e) {
+                    // do nothing
+                } finally {
+                    finish();
+                    startActivity(new Intent("com.pandaftp.main.Testshell"));
+                    stop();
+                		
+                }
+            }
+        };
+        splashTread.start();
+        //startActivity(new Intent("com.pandaftp.main.Testshell"));
     }
+    public boolean onTouchEvent(MotionEvent event) {
+	    if (event.getAction() == MotionEvent.ACTION_DOWN) {
+	        _active = false;
+	    }
+	    return true;
+	}
 }
